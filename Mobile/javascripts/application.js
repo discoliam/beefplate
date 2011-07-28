c68e10d565e8e8b5453d51afbf9c205da75df67d
Liam Richardson <discoliam@googlemail.com>
2011-07-28 11:29:28 +0100

    added mobile beefplate

---
+++ Mobile/javascripts/application.js
@@ -0,0 +1,95 @@
+// -------------- APPLICATION.JS --------------
+// Aplication wide javascript
+
+
+// -------------- CUFON --------------
+// Cufon.replace('', {
+//   hover: 'true',
+//   fontFamily: ''
+// });
+
+
+// -------------- SWFOBJECT --------------
+// var flashvars = {};
+// var params = {};
+// var attributes = {};
+// swfobject.embedSWF("/flash/", "flash-anni", "420", "90", "9.0.0", flashvars, params, attributes);
+
+
+
+// -------------- JQUERY START --------------
+$(document).ready(function () {
+
+// STYLING FIX'S
+// IE FIX'S
+
+}); //eo: Document Ready
+
+
+
+
+
+
+// -------------- COMMON FUNCTIONS/TOOLS/PLUGINS --------------
+
+// SLIDES.JS SLIDE SHOW
+
+// $(function(){
+//   $('.hero-panel').slides({
+//   preload: true,
+//   preloadImage: '/images/loading.gif',
+//   effect:'slide',
+//   hoverPause: false,
+//   generatePagination: false,
+//   slideSpeed: 800,
+//   generateNextPrev: true
+//   });
+//   $('.js-needed').hide();
+// });
+
+
+
+// ADDING BODY IMAGES
+// $("#body a.lightbox").each(function(){
+//   $(this).append('<span class="overlay">&nbsp;</span>');
+//  });
+ 
+// $("#body a").each(function(){
+//   image = $(this);
+//   image.wrap('<a class="lightbox image-wrap" target="_blank" title="'+ image.attr('title') +'" href="'+   image.attr('longdesc') +'" />');
+//   image.after('<span class="zoom">&nbsp;</span>');
+// });
+
+
+// FANCYBOX
+// $('').fancybox({
+//   'padding'        : 10,
+//   'autoScale'      : true,
+//   'transitionIn'    : 'elastic',
+//   'transitionOut'    : 'elastic',
+//   'title'          : false,
+//   'width'        : 680,
+//   'height'        : 495,
+//   'centerOnScroll'   : true,
+//   'titlePosition' : 'over'
+// });
+
+// JCAROUSEL LITE 
+// $(".component.gallery.single.wide .carousel").each(function(index, gallery) {
+//   gallery = $(gallery);
+//   if (gallery.find('li').length > 3) {
+//           
+//     gallery.parents(".carousel-wrap").css("margin","10px 70px 0 50px");
+//     gallery.before("<a class='carousel-back'>Backward</a>");
+//     gallery.after("<a class='carousel-forward'>Forward</a>");
+// 
+//     gallery.jCarouselLite({
+//       btnNext: gallery.next(".carousel-forward"),
+//       btnPrev: gallery.prev(".carousel-back"),
+//       visible: 3
+//     });
+//   }
+// });
+
+// FLASH MESSAGES
+// $('#flash').delay(4000).slideUp(400);
